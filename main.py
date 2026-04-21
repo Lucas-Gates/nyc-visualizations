fbbc5cc9f6c2d03c0789cdc40ef4cc40253ab45f
Kz1nk <kylefredzink@gmail.com>
2026-04-21 14:43:54 -0500

    Basic data loading

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ print("--------------------------\n\n")
 #--------------------------------#
 # PART 1: Load & Inspect Raw Data
 #--------------------------------#
+
+df = pd.read_csv('Motor_Vehicle_Collisions_-_Crashes_20260421.csv')
+
+# First 5 rows
+print("First 5 Rows: \n")
+print(df.head(), "\n")
+
+# Number of rows & columns
+print(f"Total Number of Rows: {df.shape[0]}\nTotal Number of Columns: {df.shape[1]} \n")
+
+# Column names
+print("Column Names: ", df.columns, "\n")
+
+# Data types
+print("Data Types:\n", df.dtypes.to_string(), "\n")
