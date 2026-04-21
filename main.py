6010692a3a6e6bc800281c5cbb12191758a4310b
Kz1nk <kylefredzink@gmail.com>
2026-04-21 14:29:06 -0500

    Brief start for main.

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+#------#
+# START
+#------#
+
+print("\n\n--------------------------\n")
+print("CS_2300 - Final Project\n")
+print("--------------------------\n\n")
+
+
+#--------------------------------#
+# PART 1: Load & Inspect Raw Data
+#--------------------------------#
