7fef469ad35ed46bcc71af6986454cbd4ae455b4
zenmitchell41 <zenmitchell41@gmail.com>
2026-04-21 14:45:56 -0500

    cleaning data

---
+++ clean.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+data = pd.read_csv("./data/Motor_Vehicle_Collisions_-_Crashes_20260421.csv")
+
+print(data.head())
+print(f"\nThe data has {len(data)} rows and {len(data.columns)} columns\n")
+print("Column Names: ")
+for c in data.columns:
+    print(f"\t{c}")
+print("\n")
+print(data.dtypes)
+print("\n")
+
+
+data = data[data['LATITUDE'].notna() & data['LONGITUDE'].notna()]
+
+print("Missing Values:")
+print(data.isna().sum())
+
+
+
