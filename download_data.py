abe33226ffb0ef84f1cc643d2176ac5586297dde
Lucas-Gates <lucascgates@gmail.com>
2026-04-29 10:47:59 -0500

    download script now makes the data folder too

--- download_data.py
+++ download_data.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import os
 
 #url filtered to 2025
 url = "https://data.cityofnewyork.us/resource/h9gi-nx95.csv?$where=crash_date%20between%20'2025-01-01T00:00:00'%20and%20'2025-12-31T23:59:59'&$limit=5000000"
+
+#make data folder if it doesn't exist
+if not os.path.exists("data"):
+    os.makedirs("data")
 
 #save location
 output_path = "data/new_DATA.csv"
