420a6778b9cb8df6a2071a067568cab8e9a20b0c
Kz1nk <kylefredzink@gmail.com>
2026-04-30 13:40:27 -0500

    Implemented part 3, distribution.

--- main.py
+++ main.py
@@ -167,6 +167,52 @@ print("\n-------------------------------------------------\n")
 print("PART 3: Distribution Visualization — Crashes by Hour\n")
 print("-------------------------------------------------\n")
 
+#12 hour labels
+def format_hour(h):
+    if h == 0:
+        return "12 AM"
+    elif h < 12:
+        return f"{h} AM"
+    elif h == 12:
+        return "12 PM"
+    else:
+        return f"{h - 12} PM"
+hour_labels = [format_hour(h) for h in range(24)]
+
+#plot
+plt.figure(figsize=(11, 5))
+colors = {
+    "Property Damage": "blue",
+    "Injury": "orange",
+    "Fatal": "red"
+}
+plot_order = ["Property Damage", "Injury", "Fatal"]
+for severity in plot_order:
+    subset = df_clean[df_clean["SEVERITY_LABEL"] == severity]
+    counts = subset["HOUR"].value_counts().sort_index()
+    counts = counts.reindex(range(24), fill_value=0)
+    percent = counts / counts.sum() * 100
+    smooth = percent.rolling(window=3, center=True, min_periods=1).mean()
+    plt.fill_between(range(24), smooth, alpha=0.15, color=colors[severity])
+    plt.plot(range(24), smooth, label=severity, color=colors[severity], linewidth=2)
+
+#axis formatting
+plt.xticks(range(24), hour_labels, rotation=45)
+plt.xlim(0, 23)
+plt.margins(x=0)
+
+#labels and legend
+plt.title("Crashes by Percent by Hour and Severity")
+plt.xlabel("Time of Day")
+plt.ylabel("Percent of Crashes")
+plt.grid(alpha=0.3, linestyle="--")
+plt.legend(title="Crash Severity")
+
+#save and show
+plt.tight_layout()
+plt.savefig("fig1_distribution_danger_window.png")
+plt.show()
+
 
 #-------------------------------------------------#
 # PART 4: Relationship Visualization - Crashes by Hour
@@ -222,8 +268,8 @@ plt.figtext(0.01, 0.02, "Shaded bands = morning (7–9 AM) and evening (4–6 PM
 
 plt.tight_layout()
 
-plt.savefig('fig1_crashes_by_hour.png')
-print("Figure saved as 'fig1_crashes_by_hour.png'\n")
+plt.savefig('fig3_aggregate_crashes_by_hour.png')
+print("Figure saved as 'fig3_aggregate_crashes_by_hour.png'\n")
 
 plt.show()
 
