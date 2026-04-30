8da4b6952db41eb593ef4bc70b43965b38bd6bdf
Lucas-Gates <lucascgates@gmail.com>
2026-04-30 14:54:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,48 @@
-import runpy
 from clean_data import load_and_clean_data
+
+from Dist_1 import dist1Plot
+from Dist_2 import dist2Plot
 from relationships import relPlot
+
 
 print("\n\n--------------------------")
 print("CS_2300 - Final Project")
 print("--------------------------\n")
 
-df_clean = None
+print("Cleaning data...")
+df_clean = load_and_clean_data()
 
 while True:
     print("\nCONTROL PANEL")
-    print("1. Clean data")
-    print("2. Run Dist_1 visualization")
-    print("3. Run Dist_2 visualization")
-    print("4. Run relationships visualization")
-    print("5. Run all visualizations")
-    print("6. Exit")
+    print("1. Run Dist_1 visualization")
+    print("2. Run Dist_2 visualization")
+    print("3. Run relationships visualization")
+    print("4. Run all visualizations")
+    print("0. Quit")
 
     choice = input("\nEnter your choice: ")
 
     if choice == "1":
-        df_clean = load_and_clean_data()
+        dist1(df_clean)
 
     elif choice == "2":
-        if df_clean is None:
-            df_clean = load_and_clean_data()
-
-        runpy.run_path("Dist_1.py")
+        dist2P(df_clean)
 
     elif choice == "3":
-        if df_clean is None:
-            df_clean = load_and_clean_data()
-
-        runpy.run_path("Dist_2.py", init_globals={"df_clean": df_clean})
+        fig = relPlot(df_clean)
+        fig.show()
 
     elif choice == "4":
-        fig = relPlot()
+        plot = dist1Plot(df_clean)
+        plot.show()
+
+        plot = dist2Plot(df_clean)
+        plot.show()
+
+        fig = relPlot(df_clean)
         fig.show()
 
-    elif choice == "5":
-        if df_clean is None:
-            df_clean = load_and_clean_data()
-
-        runpy.run_path("Dist_1.py")
-        runpy.run_path("Dist_2.py", init_globals={"df_clean": df_clean})
-
-        fig = relPlot()
-        fig.show()
-
-    elif choice == "6":
+    elif choice == "0":
         print("\nEND OF PROGRAM\n")
         break
 
